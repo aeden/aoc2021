6d8c09cb61feeae2630984cf8acc766d63c79b1f
Anthony Eden <me@anthonyeden.com>
2021-12-02 11:43:35 +0100

    Complete part 2 for day 2

---
+++ day2/p2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector<string> read_data(string filename) {
+  ifstream infile;
+  infile.open(filename);
+  vector<string> data;
+
+  if (infile.is_open()) {
+    string line;
+    while(getline(infile, line)) {
+      data.push_back(line);
+    }
+    return data;
+  } else {
+    cout << "Failed to read data" << endl;
+    return data;
+  }
+}
+
+const string FORWARD = "forward";
+const string UP = "up";
+const string DOWN = "down";
+
+int main(int argc, char** argv) {
+  string filename;
+  if (argc != 2) {
+    cout << "Usage: " << argv[0] << " datafile" << endl;
+    return 1;
+  } else {
+    filename = argv[1];
+  }
+
+  vector<string> data = read_data(filename);
+  cout << "Loaded " << data.size() << " data lines" << endl;
+
+  int horizontal_position = 0;
+  int depth = 0;
+  int aim = 0;
+
+  for (int i = 0; i < data.size(); i++) {
+    vector<string> tokens;
+    string row = data[i];
+    stringstream tokenizer(row);
+    string token;
+    while (tokenizer >> token) {
+      tokens.push_back(token);
+    }
+    string command = tokens[0];
+    int value = stoi(tokens[1]);
+
+    if (command.compare(FORWARD) == 0) {
+      horizontal_position += value; 
+      depth += aim * value;
+    } else if (command.compare(UP)) {
+      aim += value;
+    } else if (command.compare(DOWN)) {
+      aim -= value;
+    }
+  }
+
+  cout << "Horizontal position: " << horizontal_position << endl;
+  cout << "Depth: " << depth << endl;
+  cout << "Result: " << horizontal_position * depth << endl;
+
+  cout << "Done" << endl;
+  return 0;
+}

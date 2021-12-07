2a69318bdd03c088db3dd9ee1c6cfd92c70abac0
Anthony Eden <me@anthonyeden.com>
2021-12-07 08:43:36 +0100

    Day 1, part 1

---
+++ day7/src/p1.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "lib/io.h"
+
+#include <set>
+#include <map>
+
+using namespace std;
+
+class Crab {
+  public:
+    int starting_position;
+
+    Crab(int p) {
+      starting_position = p;
+    }
+
+    // calculate the cost for the crab to reach position p
+    int cost(int p) {
+      return abs(p - starting_position);
+    }
+};
+
+int main (int argc, char** argv) {
+  string filename;
+  int days;
+  if (argc != 2) {
+    cout << "Usage: " << argv[0] << " datafile" << endl;
+    return 1;
+  } else {
+    filename = argv[1];
+  }
+
+  vector<string> data = read_data(filename);
+  cout << "Loaded " << data.size() << " data lines" << endl;
+
+  vector<int> starting_positions;
+  parse_ints(data[0], &starting_positions, ',');
+  print_ints(&starting_positions, ',');;
+
+  vector<Crab> crabs;
+  set<int> positions;
+
+  for (vector<int>::iterator iter = starting_positions.begin(); iter != starting_positions.end(); iter++) {
+    crabs.push_back(Crab(*iter));
+  }
+
+  int min_position = *min_element(starting_positions.begin(), starting_positions.end());
+  int max_position = *max_element(starting_positions.begin(), starting_positions.end());
+
+  for (int i = min_position; i < max_position + 1; i++) {
+    positions.insert(i);
+  }
+
+  map<int,int> cost_map;
+  for (set<int>::iterator iter = positions.begin(); iter != positions.end(); iter++) {
+    int p = *iter;
+    // cout << p;
+    // if (next(iter) != positions.end()) cout << ",";
+
+    for (vector<Crab>::iterator crab_iter = crabs.begin(); crab_iter != crabs.end(); crab_iter++) {
+      int cost = crab_iter -> cost(p);
+      if (cost_map.find(p) == cost_map.end()) {
+        cost_map[p] = cost;
+      } else {
+        cost_map[p] += cost;
+      }
+    }
+  }
+  // cout << endl;
+
+  int lowest_cost = 0;
+  int lowest_cost_position = 0;
+  // cout << "Costs:" << endl;
+  for (map<int,int>::iterator iter = cost_map.begin(); iter != cost_map.end(); iter++) {
+    int p = iter->first;
+    int cost = iter->second;
+    if (lowest_cost == 0 || cost < lowest_cost) {
+      lowest_cost = cost;
+      lowest_cost_position = p;
+    }
+    // cout << p << "=" << cost << endl;
+  }
+
+  cout << "Lowest cost " << lowest_cost << " is at position " << lowest_cost_position << endl;
+
+  cout << "Done" << endl;
+}

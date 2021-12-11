9695eb0f6bf83b1dfe4f5479b953b18d72d34ba6
Anthony Eden <me@anthonyeden.com>
2021-12-11 16:32:46 +0100

    Day 11, part 2 implemented

---
+++ day11/src/p2.cpp
@@ -0,0 +1,257 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "lib/io.h"
+
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+const int FLASH_TRIGGER = 9;
+
+class Point {
+  public:
+    int x;
+    int y;
+
+    Point() { }
+
+    Point(int x, int y) {
+      this->x = x;
+      this->y = y;
+    }
+
+    string to_string() {
+      return std::to_string(x) + "," + std::to_string(y);
+    }
+
+    bool operator==(Point p) {
+      return p.x == x && p.y == y;
+    }
+
+    bool is_valid() {
+      return x >= 0 && x <= 9 && y >= 0 && y <= 9;
+    }
+};
+
+class Octopus {
+  public:
+    Point location;
+    int energy;
+
+    Octopus(int energy, Point location) {
+      this->energy = energy;
+      this->location = location;
+    }
+
+    string to_string() {
+      stringbuf buffer;
+      ostream os (&buffer);
+      os << location.to_string() << ":" << std::to_string(energy);
+      return buffer.str();
+    }
+
+    bool operator==(Octopus other) {
+      return this->location == other.location;
+    }
+};
+
+class Flashed {
+  public:
+    vector<Octopus*> *flashed;
+
+    Flashed(vector<Octopus*> *flashed) {
+      this->flashed = flashed;
+    }
+
+    bool operator()(Octopus *o) {
+      vector<Octopus*>::iterator iter = find(flashed->begin(), flashed->end(), o);
+      return iter != flashed->end();
+    }
+};
+
+class Cave {
+  public:
+    vector<vector<Octopus> > octopuses; 
+
+    bool is_synchronized() {
+      for (int i = 0; i < octopuses.size(); i++) {
+        for (int j = 0; j < octopuses[i].size(); j++) {
+          if (octopuses[i][j].energy != octopuses[0][0].energy) {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+
+    void flash(Octopus *o, vector<Octopus*> *neighbors, vector<Octopus*> *flashed) {
+      //cout << "flash " << o->to_string() << endl;
+      flashed->push_back(o);
+      for (vector<Octopus*>::iterator iter = neighbors->begin(); iter != neighbors->end(); iter++) {
+        Octopus *n = *iter;
+        n->energy += 1;
+        if (n->energy > FLASH_TRIGGER && !Flashed(flashed)(n)) {
+          //cout << "flash needed for " << n->to_string() << endl;
+          vector<Octopus*> next_neighbors = find_neighbors(n, flashed);          
+          flash(n, &next_neighbors, flashed);
+        }
+      }
+    }
+
+    int step(int n) {
+      cout << "Step " << n << endl;
+      for (int i = 0; i < octopuses.size(); i++) {
+        for (int j = 0; j < octopuses[i].size(); j++) {
+          octopuses[i][j].energy += 1;
+        }
+      }
+
+      cout << "Flashing" << endl;
+      vector<Octopus*> flashed;
+      for (int i = 0; i < octopuses.size(); i++) {
+        for (int j = 0; j < octopuses[i].size(); j++) {
+          if (octopuses[i][j].energy > FLASH_TRIGGER && !Flashed(&flashed)(&octopuses[i][j])) {
+            vector<Octopus*> neighbors = find_neighbors(&octopuses[i][j], &flashed);
+            flash(&octopuses[i][j], &neighbors, &flashed); 
+          }
+        }
+      }
+
+      cout << "Setting all flashed energy to 0" << endl;
+      for (vector<Octopus*>::iterator iter = flashed.begin(); iter != flashed.end(); iter++) {
+        Octopus *o = *iter;
+        o->energy = 0;
+      }
+
+      return flashed.size();
+    }
+
+    void print_octopuses(vector<Octopus*> *octopuses) {
+      for (vector<Octopus*>::iterator iter = octopuses->begin(); iter != octopuses->end(); iter++) {
+        Octopus *o = *iter;
+        cout << o->to_string();
+        if (next(iter) != octopuses->end()) cout << " | ";
+      }
+      cout << endl;
+    }
+
+    void configure(vector<string> data) {
+      for (int y = 0; y < data.size(); y++) {
+        vector<Octopus> line_octopuses;
+        for (int x = 0; x < data[y].size(); x++) {
+          int v = data[y][x] - '0';
+          Point p = Point(x, y);
+          Octopus o = Octopus(v, p);
+          line_octopuses.push_back(o);
+        }
+        octopuses.push_back(line_octopuses);
+      }
+    }
+
+    vector<Octopus*> find_neighbors(Octopus *o, vector<Octopus*> *flashed) {
+      vector<Octopus*> neighbors; 
+      Point l = o->location;
+
+      Point left = Point(l.x - 1, l.y);
+      if (left.is_valid()) {
+        Octopus* n = &octopuses[left.y][left.x];
+        neighbors.push_back(n);
+      }
+
+      Point right = Point(l.x + 1, l.y);
+      if (right.is_valid()) {
+        Octopus* n = &octopuses[right.y][right.x];
+        neighbors.push_back(n);
+      }
+
+      Point up = Point(l.x, l.y - 1);
+      if (up.is_valid()) {
+        Octopus* n = &octopuses[up.y][up.x];
+        neighbors.push_back(n);
+      }
+
+      Point down = Point(l.x, l.y + 1);
+      if (down.is_valid()) {
+        Octopus* n = &octopuses[down.y][down.x];
+        neighbors.push_back(n);
+      }
+
+      Point left_up = Point(l.x - 1, l.y - 1);
+      if (left_up.is_valid()) {
+        Octopus* n = &octopuses[left_up.y][left_up.x];
+        neighbors.push_back(n);
+      }
+
+      Point right_up = Point(l.x + 1, l.y - 1);
+      if (right_up.is_valid()) {
+        Octopus* n = &octopuses[right_up.y][right_up.x];
+        neighbors.push_back(n);
+      }
+
+      Point left_down = Point(l.x - 1, l.y + 1);
+      if (left_down.is_valid()) {
+        Octopus* n = &octopuses[left_down.y][left_down.x];
+        neighbors.push_back(n);
+      }
+
+      Point right_down = Point(l.x + 1, l.y + 1);
+      if (right_down.is_valid()) {
+        Octopus* n = &octopuses[right_down.y][right_down.x];
+        neighbors.push_back(n);
+      }
+
+      return neighbors;
+    }
+
+    string to_string() {
+      stringbuf buffer;
+      ostream os (&buffer);
+      for (int i = 0; i < octopuses.size(); i++) {
+        for (int j = 0; j < octopuses[i].size(); j++) {
+          os << octopuses[i][j].energy;
+        }
+        os << endl;
+      }
+      return buffer.str();
+    }
+
+};
+
+
+int main (int argc, char** argv) {
+  string filename;
+  if (argc != 2) {
+    cout << "Usage: " << argv[0] << " datafile steps" << endl;
+    return 1;
+  } else {
+    filename = argv[1];
+  }
+
+  vector<string> data = read_data(filename);
+  cout << "Loaded " << data.size() << " data lines" << endl;
+  
+  Cave cave; 
+  
+  cout << "Configuring cave" << endl;
+  cave.configure(data); 
+ 
+  bool synchronized = false;
+  int step = 0;
+  cout << "Executing steps" << endl;
+  while (!synchronized) {
+    step++;
+    // cout << "Step " << i << endl;
+    cave.step(step);
+    cout << "After step " << step << endl;
+    cout << cave.to_string() << endl;
+
+    synchronized = cave.is_synchronized();
+  }
+
+  cout << "Result: " << step << endl;
+  cout << "Done" << endl;
+
+}
